3aa0abd679ced0bf001102a13c263ed07511a023
Joshua Nelson <jyn514@gmail.com>
2020-02-01 13:07:41 -0500

    use typed errors

--- src/data/error.rs
+++ src/data/error.rs
@@ -135,6 +135,9 @@ pub enum SemanticError {
     #[error("expected expression, got typedef")]
     TypedefInExpressionContext,
 
+    #[error("initializers cannot be empty")]
+    EmptyInitializer,
+
     #[doc(hidden)]
     #[error("internal error: do not construct nonexhaustive variants")]
     __Nonexhaustive,
